ea3a82d2affd8e0acc3a61f5b0d266619f044a03
James Lee <lizihao945@gmail.com>
2014-02-06 15:49:46 +0800

    sf

--- matcher.c
+++ matcher.c
@@ -11,9 +11,27 @@
  * to reasonably short, null-terminated strings.
  */
 
+int equal(char c, char *pattern) {
+	if (*(pattern-1) == '\\') {
+		if (*pattern == c)
+			return 1;
+		else
+			return 0;
+	}
+	switch(*pattern) {
+		case '.':
+			return 1;
+		default:
+			if (c == *pattern)
+				return 1;
+			else
+				return 0;
+	}
+}
+
 int match_bracket(char *line, char *pattern) {
 	int m, n, count = 0;
-	char c = *(pattern-1);
+	char *tmp = pattern;
 	char *idx = strchr(pattern, ',');
 	*idx = '\0';
 	m = atoi(pattern+1);
@@ -21,7 +39,7 @@ int match_bracket(char *line, char *pattern) {
 	idx = strchr(pattern, '}');
 	*idx = '\0';
 	n = atoi(pattern);
-	while(*line == c)
+	while(equal(*line, tmp-1))
 		count++;
 	// only m available
 	if (count >= m && n == 0)
@@ -33,18 +51,32 @@ int match_bracket(char *line, char *pattern) {
 }
 
 int rgrep_matches(char *line, char *pattern) {
-  for (;*pattern != '\0'; pattern++) {
-  	switch (*pattern) {
-  		case '.':
-  			line++;
-  			break;
-  		case '{':
-  			if (match_bracket(line, pattern))
-  				break;
-  			else
-  				return 0;
-  			break;
-  	}
-  }
+	char *end;
+	for (;*line != '\0'; line++) {
+		end = line;
+	  for (;*pattern != '\0'; pattern++) {
+	  	switch (*pattern) {
+	  		case '.':
+	  			end++;
+	  			break;
+	  		case '{':
+	  			if (match_bracket(end, pattern))
+	  				break;
+	  			else
+	  				continue;
+	  			break;
+	  		case '\\':
+	  			pattern++;
+	  			if (*end == *pattern)
+	  				end++;
+	  			else
+	  				continue;
+	  			break;
+	  		default:
+	  		 end++;
+	  		 break;
+	  	}
+	  }
+	}
   return 1;
 }
