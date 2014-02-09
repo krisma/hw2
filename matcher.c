5f6616ee4f1b4d26d8bda3b0f77ab1fe0afb7a8b
James Lee <lizihao945@gmail.com>
2014-02-09 20:17:33 +0800

    bug fixed & tests passed

--- matcher.c
+++ matcher.c
@@ -4,29 +4,49 @@
 #include "matcher.h"
 
 
-int bracket_match(char last, char *tmp_line, char *tmp_pattern) {
-	int m, n, i, j, no_problem;
-	char *tmp;
+int bracket_match(char *tmp_line, char *tmp_pattern) {
+	int m, n, i, j, no_problem, period_flag;
+	char *tmp, *tmp2, pre, *next;
+	// m
 	tmp = strchr(tmp_pattern, ',');
 	*tmp = '\0';
 	m = atoi(tmp_pattern + 1);
 	*tmp = ',';
-	tmp_pattern = tmp;
-	tmp = strchr(tmp_pattern, '}');
-	*tmp = '\0';
-	n = atoi(tmp_pattern + 1);
-	*tmp = '}';
-	tmp_pattern = tmp + 1;
+	// n
+	if (*(tmp + 1) == '}') {
+		// minus one because of '\n'
+		n = strlen(tmp_line) - m - 1;
+	} else {
+		tmp2 = strchr(tmp, '}');
+		*tmp2 = '\0';
+		n = atoi(tmp + 1);
+		*tmp2 = '}';
+	}
+	// pre
+	period_flag = 0;
+	if (*(tmp_pattern - 2) == '\\')
+		pre = *(tmp_pattern - 1);
+	else {
+		if (*(tmp_pattern - 1) == '.')
+			period_flag = 1;
+		pre = *(tmp_pattern - 1);
+	}
+	// printf("Ready to do bracket match: char: %c; m: %d; n : %d\nline is %s", pre, m, n, tmp_line);
 	for (i = m; i <= m + n; i++) {
 		// try if c appears i times in line
 		no_problem = 1;
-		for (j = 0; j < i; j++)
-			if (*(tmp_line + j) != last) {
-				no_problem = 0;
-				break;
+		if (!period_flag)
+			// if pre is not a '.', do the check
+			for (j = 0; j < i; j++) {
+				if (*(tmp_line + j) != pre) {
+					no_problem = 0;
+					break;
+				}
 			}
-		if (no_problem)
-			no_problem = rgrep_matches(tmp_line, tmp_pattern);
+		if (no_problem) {
+			next = strchr(tmp_pattern, '}') + 1;
+			no_problem = rgrep_matches(tmp_line + i, next);
+		}
 		if (no_problem) return 1;
 	}
 	return 0;
@@ -40,52 +60,49 @@ int bracket_match(char last, char *tmp_line, char *tmp_pattern) {
  * to reasonably short, null-terminated strings.
  */
 int rgrep_matches(char *line, char *pattern) {
-		char *tmp_line, *tmp_pattern, last;
-		int no_problem;
-		printf("Ready to match:%swith pattern: %s\n", line, pattern);
-		for (; *(line+1) != '\0'; line++) {
-			// iterate to try different entries
-			tmp_line = line;
-			// if no problem found when pattern ends, return true
-			no_problem = 1;
-			tmp_pattern = pattern;
-			while(*tmp_pattern != '\0') {
-				//printf("next pattern: %c(%d)\n", *tmp_pattern, *tmp_pattern);
-				if (*tmp_line == '\0') {
-					no_problem = 0;
+	char *tmp_line, *tmp_pattern;
+	int no_problem;
+	if (*pattern == '\0') return 1;
+	//printf("Ready to match:%swith pattern: %s\n", line, pattern);
+	while (*line != '\0') {
+		// iterate to try different entries
+		tmp_line = line;
+		// if no problem found when pattern ends, return true
+		no_problem = 1;
+		tmp_pattern = pattern;
+		while(*tmp_pattern != '\0') {
+			//printf("next pattern: %c(%d)\n", *tmp_pattern, *tmp_pattern);
+			if (*tmp_line == '\0') {
+				no_problem = 0;
+				break;
+			}
+			switch (*tmp_pattern) {
+				case '.':
+					if (*(tmp_pattern + 1) == '{')
+						break;
+					tmp_line++;
 					break;
-				}
-				switch (*tmp_pattern) {
-					case '.':
-						if (*(tmp_pattern + 1) == '{')
-							break;
-						last = *tmp_pattern;
-						tmp_line++;
+				case '{':
+					if (bracket_match(tmp_line, tmp_pattern))
+						return 1;
+					break;
+				case '\\':
+					tmp_pattern++;
+					// do not break
+				default:
+					if (*(tmp_pattern + 1) == '{')
 						break;
-					case '{':
-						no_problem = bracket_match(last, tmp_line, tmp_pattern);
-						break;
-					case '\\':
-						tmp_pattern++;
-						// do not break
-					default:
-						if (*(tmp_pattern + 1) == '{')
-							break;
-						//printf("%c %c\n", *tmp_line, *tmp_pattern);
-						if (*tmp_line != *tmp_pattern)
-							no_problem = 0;
-						last = *tmp_pattern;
-						tmp_line++;
-				}
-				// if problem found, quit the loop
-				if (!no_problem) break;
-				tmp_pattern++;
+					if (*tmp_line != *tmp_pattern)
+						no_problem = 0;
+					tmp_line++;
 			}
-			// if no problem found, return true
-			if (no_problem) {
-				printf("Success!\n");
-				return 1;
-			}
+			// if problem found, quit the loop
+			if (!no_problem) break;
+			tmp_pattern++;
 		}
-    return 0;
+		// if no problem found, return true
+		if (no_problem) return 1;
+		line++;
+	}
+  return 0;
 }
